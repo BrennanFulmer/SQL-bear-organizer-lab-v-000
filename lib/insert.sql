e14163837a8bed69b6b720df9718f75611660078
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-27 05:17:06 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ('Mr. Chocolate', 19, 'F', "polar", "mixed", 1);
+INSERT INTO bears VALUES ('Mr. Chocolate', 19, 'F', "polar", "mixed", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ('Rowdy', 75, 'M', "brown", "calm", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
