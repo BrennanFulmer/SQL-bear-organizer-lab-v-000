f69c1ad9031cd884e92932dbf9e814880bd032f0
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-27 05:20:00 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
-('Mr. Chocolate', 19, 'F', "polar", "mixed", 1);
-('Rowdy', 75, 'M', "brown", "calm", 1);
-('Tabitha', 6, 'M', "black", "rowdy", 1);
-('Sergeant Brown', 11, 'F', "off-white", "mousey", 1);
-('Melissa', 11, 'M', "dark brown", "good", 1);
-('Grinch', 44, 'F', "purple", "conflicted", 1);
-('Wendy', 1, 'M', "green", "fine", 1);
+('Mr. Chocolate', 19, 'F', "polar", "mixed", 1),
+('Rowdy', 75, 'M', "brown", "calm", 1),
+('Tabitha', 6, 'M', "black", "rowdy", 1),
+('Sergeant Brown', 11, 'F', "off-white", "mousey", 1),
+('Melissa', 11, 'M', "dark brown", "good", 1),
+('Grinch', 44, 'F', "purple", "conflicted", 1),
+('Wendy', 1, 'M', "green", "fine", 1),
 (null, 3, 'M', "eggshell", "angry", 0);
 
 
